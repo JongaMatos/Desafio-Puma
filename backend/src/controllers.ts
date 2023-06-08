3d2dae000a37ec29d62355d9caa587dd1b2ceb21
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-06-08 16:17:06 -0300

    Adds user removal controller (delete).

--- backend/src/controllers.ts
+++ backend/src/controllers.ts
@@ -1,6 +1,6 @@
-import { save, load } from "./fileManipulation";
-import { Request, Response } from "express-serve-static-core";
+import { Request, Response } from "express";
 
+import { save, load } from "./fileManipulation";
 
 interface UserData {
     username: string,
@@ -10,15 +10,13 @@ interface UserData {
 }
 
 
-
-
 const getAll = (req: Request, res: Response) => {
     try {
 
         const data = load("data.txt");
         if (data === undefined)
             return res.status(200).send({ users: {} });
-        return res.status(200).send({ data });
+        return res.status(200).send(data);
 
     } catch (error) {
 
@@ -35,8 +33,8 @@ const addUser = (req: Request, res: Response) => {
         const data = load("data.txt")
 
         if (data === undefined) {
-            save("data.txt", { users: [{ username, nome, avatar, url }] });
-            return res.status(200).send({ users: [{ username, nome, avatar, url }] });
+            save("data.txt", { users: [{ username, nome, avatar, url, star: false }] });
+            return res.status(200).send({ users: [{ username, nome, avatar, url, star: false }] });
         }
         // console.log(data.users.findIndex((user: UserData) => user.username == username))
         if (data.users.findIndex((user: UserData) => user.username == username) >= 0)
@@ -44,18 +42,46 @@ const addUser = (req: Request, res: Response) => {
 
         var newData = data.users;
 
-        newData.push({ username, nome, avatar, url });
+        newData.push({ username, nome, avatar, url, star: false });
         save("data.txt", { users: newData });
         return res.status(200).send({ users: newData });
-
 
     } catch (error) {
 
         // console.log({ error });
         return res.status(400).send({ message: "não foi possivel adicionar usuario" });
+    }
+}
+
+const removeOne = (req: Request, res: Response) => {
+
+    const { username } = req.params;
+
+    try {
+
+        let { users } = load("data.txt");
+        if (users === undefined || username === undefined)
+            return res.status(400).send({ message: "Não foi possivel remover usuário." });
+
+        const index = users.findIndex((user: UserData) => user.username == username);
+
+        if (index < 0)
+            return res.status(404).send({ message: "Usuário não registrado." });
+
+        users.splice(index, 1);
+        save("data.txt", { users });
+        return res.status(200).send({ users });
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send({ message: "Não foi possivel remover usuário." });
 
     }
 
+
+
+
 }
 
-export { getAll, addUser }
+export { getAll, addUser, removeOne }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,34 +1,30 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
-import { save, load } from "./fileManipulation";
-import { addUser, getAll } from "./controllers";
+import { addUser, getAll ,removeOne} from "./controllers";
 
 const router = Router();
 
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
 
     return res.status(200).send({ message: "Servidor rodando na porta 5000" })
 
 });
 
-router.post("/users", (req, res) => {
+router.post("/users", (req: Request, res: Response) => {
     addUser(req, res);
 });
 
-router.get("/users", (req, res) => {
+router.get("/users", (req: Request, res: Response) => {
     getAll(req, res);
 });
 
+router.delete("/users/:username", (req: Request, res: Response) => {
 
-
-router.delete("/users", (req, res) => {
-
-    return res.status(200).send({ message: "TODO" })
-
+    removeOne(req, res);
 });
 
-router.patch("/users", (req, res) => {
+router.patch("/users", (req: Request, res: Response) => {
 
     return res.status(200).send({ message: "TODO" })
 
