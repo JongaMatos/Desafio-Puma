30e2ea400b96892482c340495e8f4e514a84e216
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-06-09 04:15:00 -0300

    Changes Modal library.

--- frontend/src/components/modal/modal.tsx
+++ frontend/src/components/modal/modal.tsx
@@ -1,25 +1,23 @@
 import React, { useContext } from 'react';
-import Modal from "react-overlays/Modal";
+// import Modal from "react-overlays/Modal";
+import Modal from "react-modal";
+
 
 import { AppContext } from '../../context';
 import './modal.css'
 
-// const renderBackdrop = (props: any) => <div className="backdrop" {...props} />;
 
 const ModalOverlay = () => {
     const { modalMessage, modalStatus, resetModal } = useContext(AppContext);
 
     return (
         <Modal
-            className='modal'
-            show={modalStatus}
-            // renderBackdrop={renderBackdrop}
+            id='modal'
+            isOpen={modalStatus}
         >
-            <div className='innerbox'>
                 <h2>{modalMessage}</h2>
 
                 <button onClick={resetModal} >Fechar</button>
-            </div>
         </Modal>
     )
 }
